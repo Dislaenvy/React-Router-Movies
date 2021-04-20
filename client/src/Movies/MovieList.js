6b0405fd6c87da71a0ca4ac2ae6c1f3649273600
Merfi Disla <merfydisla15@gmail.com>
2021-04-19 20:29:14 -0400

    Movies

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import {Link, useRouteMatch} from 'react-router-dom';
 
 export default function MovieList(props) {
   return (
     <div className="movie-list">
-      {props.movies.map(movie => (
+      {props.movies.map(movie => ( 
+        <Link key={movies.id} to={`/movies/${movie.id}`}>
         <MovieDetails key={movie.id} movie={movie} />
+        </Link>
       ))}
     </div>
   );
